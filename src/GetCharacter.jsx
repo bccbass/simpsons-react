f2531920fc697ef09ebdf6eea348b3e557363be5
Benjamin Campbell <benjamincampbell@benjamins-mini.lan>
2023-07-30 08:57:19 +1000

    fixed option for Random

--- src/GetCharacter.jsx
+++ src/GetCharacter.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 
-const characters = ['Random', 'Homer', 'Moe', 'Bart',  'Lisa', 'Marge', 'Dr. Nick', 'Duffman', 'Chief Wiggum', 'Ralph Wiggum', 'Apu', 'Principal Skinner', 'Milhouse', 'Comic Book Guy', 'Otto', 'Mayor Quimby', 'Rainier Wolfcastle']
+const characters = ['Homer', 'Moe', 'Bart',  'Lisa', 'Marge', 'Dr. Nick', 'Duffman', 'Chief Wiggum', 'Ralph Wiggum', 'Apu', 'Principal Skinner', 'Milhouse', 'Comic Book Guy', 'Otto', 'Mayor Quimby', 'Rainier Wolfcastle']
 
 const GetCharacter = ({setCharacter}) => {
   return <>
     <label id="charactersLabel" htmlFor="characters">Choose a Character </label><select name="characters" id="characters" style={{fontFamily: 'Homer Simpson Revised'}} onChange={e => setCharacter(e.target.value)}>
+    <option id="random"value="">Random</option>
         { characters.map(character => <option id={character}value={character}>{character}</option>)}                  
     </select>
 
